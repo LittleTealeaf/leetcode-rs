a223c2671eacd6aa03ccd899e8f71644d5ab1382
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-07-16 09:10:29 -0400

    q_202

---
+++ src/q_202.rs
@@ -0,0 +1,30 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {
+        let mut values = Vec::new();
+        let mut n = n;
+
+        while !values.contains(&n) {
+            values.push(n);
+            n = n
+                .to_string()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as i32)
+                .map(|d| d * d)
+                .sum();
+        }
+
+        n == 1
+    }
+}
+
+#[test]
+fn test_1() {
+    assert!(Solution::is_happy(19));
+}
+
+#[test]
+fn test_2() {
+    assert!(!Solution::is_happy(2));
+}
